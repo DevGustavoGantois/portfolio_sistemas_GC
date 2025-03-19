c70c4ae16900ae471490a1a3d1cce15586121bb0
Gustavo Gantois <gugagantois@gmail.com>
2025-03-18 21:44:27 -0300

    feat:Create carousel component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { CarouselTechs } from "./c-carousel-techs";
 import { HomeHero } from "./s-home";
 
 export default function Page() {
   return (
     <>
     <HomeHero />
+    <CarouselTechs />
     </>
   );
 }
--- src/app/v-data.ts
+++ src/app/v-data.ts
@@ -3,5 +3,41 @@ export const HomeHeroData = [
         description: "Crie sistemas robustos e preparados para o futuro! Com um código escalável, seu projeto se mantém organizado, de fácil manutenção e pronto para crescer junto com suas necessidades. Desenvolva com qualidade, eficiência e performance. 🚀",
         btnText: "Saiba mais",
         btnSeconday: "Portfolio"
+    },
+];
+
+export const carouselTecIconsData = [
+    {
+        icon: "/react.svg"
+    },
+    {
+        icon: "/node.svg"
+    },
+    {
+        icon: "/python.svg"
+    },
+    {
+        icon: "/css.svg"
+    },
+    {
+        icon: "/html.svg"
+    },
+    {
+        icon: "/php.svg"
+    },
+    {
+        icon: "/sass.svg"
+    },
+    {
+        icon: "/vue.svg"
+    },
+    {
+        icon: "/js.svg"
+    },
+    {
+        icon: "/figma.svg"
+    },
+    {
+        icon: "/angular.svg"
     }
 ]
