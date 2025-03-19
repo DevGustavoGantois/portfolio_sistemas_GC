91842342d1500038b1d9a5d0bd3dba6d79a6d43b
Gustavo Gantois <gugagantois@gmail.com>
2025-03-18 21:56:36 -0300

    feat:Create structure footer.

--- src/app/global/footer.tsx
+++ src/app/global/footer.tsx
@@ -1,7 +1,112 @@
+import Link from "next/link";
+
 export function Footer() {
     return (
-        <footer className="">
-            
+        <footer className="max-w-[1240px] mx-auto p-8 lg:p-0">
+            <nav className="flex items-center justify-around p-8 lg:p-0">
+                <div>
+
+                </div>
+                <ul className="flex flex-col items-center lg:items-start justify-center">
+                    <h1 className="text-white text-2xl">Titulo</h1>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                </ul>
+                <ul className="flex flex-col items-center lg:items-start justify-center">
+                    <h1 className="text-white text-2xl">Titulo</h1>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                </ul>
+                <ul className="flex flex-col items-center lg:items-start justify-center">
+                    <h1 className="text-white text-2xl">Titulo</h1>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                        Links
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </footer>
     )
 }
