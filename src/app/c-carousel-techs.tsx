3e71a71f20694d4782d0a25eef0c0c5da37acf96
Gustavo Gantois <gugagantois@gmail.com>
2025-03-19 19:44:05 -0300

    style:Adding border in carousel.

--- src/app/c-carousel-techs.tsx
+++ src/app/c-carousel-techs.tsx
@@ -7,7 +7,7 @@ export function CarouselTechs() {
       <h1 className="text-gray-500 text-center text-sm">
         Tecnologias que utilizamos para desenvolvimento dos nossos sistemas.
       </h1>
-      <div className="overflow-hidden p-6 lg:p-12 lg:mt-[50px] lg:mb-12 w-full">
+      <div className="overflow-hidden border-blue-500/20 border-1 p-6 lg:p-8 lg:mt-[50px] lg:mb-12 w-full">
         <div className="slideCarousel flex gap-32 whitespace-nowrap">
           {[...carouselTecIconsData, ...carouselTecIconsData].map((item, index) => (
             <Image key={index} src={item.icon} width={40} height={34} alt="" className="tech-icon" />
