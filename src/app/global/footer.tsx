2bccd7212f60a2bcbf2d5f2123b5281d1d98e0dd
Gustavo Gantois <gugagantois@gmail.com>
2025-03-18 22:42:27 -0300

    feat:Creating footer section.

--- src/app/global/footer.tsx
+++ src/app/global/footer.tsx
@@ -2,107 +2,114 @@ import Link from "next/link";
 
 export function Footer() {
     return (
-        <footer className="max-w-[1240px] mx-auto p-8 lg:p-0">
-            <nav className="flex items-center justify-around p-8 lg:p-0">
-                <div>
-
+        <footer className="max-w-[1240px] mx-auto p-8 lg:p-0 mt-40">
+            <nav className="flex flex-col lg:flex-row gap-8 items-center lg:items-start justify-around p-8 lg:p-0">
+                <div className="flex lg:items-start items-center justify-start flex-col gap-2">
+                    <h1 className="text-4xl text-[#1D0DB1]">GC Technology</h1>
+                    <p className="text-gray-500 text-center lg:text-start max-w-[428px]">
+                    Desenvolvemos soluções e sistemas digitais de alta performance, 
+                    totalmente escaláveis e orientados à inovação.
+                     Nosso compromisso é criar 
+                    tecnologias que impulsionam negócios, 
+                    elevam a eficiência e fazem a diferença no mercado de software.
+                    </p>
                 </div>
-                <ul className="flex flex-col items-center lg:items-start justify-center">
-                    <h1 className="text-white text-2xl">Titulo</h1>
-                    <li>
-                        <Link href="">
-                        Links
+                <ul className="flex flex-col items-center lg:items-start h-full  justify-start gap-2">
+                    <h1 className="text-white text-2xl font-medium mb-3">Home</h1>
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
+                        <Link href="#Home">
+                        Home
                         </Link>
                     </li>
-                    <li>
-                        <Link href="">
-                        Links
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
+                        <Link href="#Services">
+                        Serviços
                         </Link>
                     </li>
-                    <li>
-                        <Link href="">
-                        Links
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
+                        <Link href="#Solutions">
+                        Soluções
                         </Link>
                     </li>
-                    <li>
-                        <Link href="">
-                        Links
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
+                        <Link href="#Portfolio">
+                        Portfolio
                         </Link>
                     </li>
-                    <li>
-                        <Link href="">
-                        Links
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
+                        <Link href="#Testimonials">
+                        Depoimentos
                         </Link>
                     </li>
-                    <li>
-                        <Link href="">
-                        Links
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
+                        <Link href="#AboutUs">
+                        Sobre nós
                         </Link>
                     </li>
-                    <li>
-                        <Link href="">
-                        Links
-                        </Link>
-                    </li>
-                </ul>
-                <ul className="flex flex-col items-center lg:items-start justify-center">
-                    <h1 className="text-white text-2xl">Titulo</h1>
-                    <li>
-                        <Link href="">
-                        Links
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="">
-                        Links
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="">
-                        Links
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="">
-                        Links
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="">
-                        Links
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="">
-                        Links
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="">
-                        Links
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
+                        <Link href="#Contact">
+                        Contato
                         </Link>
                     </li>
                 </ul>
-                <ul className="flex flex-col items-center lg:items-start justify-center">
-                    <h1 className="text-white text-2xl">Titulo</h1>
-                    <li>
+                <ul className="flex flex-col items-center lg:items-start justify-center gap-2">
+                    <h1 className="text-white text-2xl font-medium mb-3">Soluções</h1>
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
                         <Link href="">
-                        Links
+                        Criação de SaaS
                         </Link>
                     </li>
-                    <li>
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
                         <Link href="">
-                        Links
+                        Websites
                         </Link>
                     </li>
-                    <li>
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
                         <Link href="">
-                        Links
+                        Design UI/UX
                         </Link>
                     </li>
-                    <li>
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
                         <Link href="">
-                        Links
+                        Automações
+                        </Link>
+                    </li>
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
+                        <Link href="">
+                        Design Criativo
+                        </Link>
+                    </li>
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
+                        <Link href="">
+                        Desenvolvimento de APIs
+                        </Link>
+                    </li>
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
+                        <Link href="">
+                        Micro Sass
+                        </Link>
+                    </li>
+                </ul>
+                <ul className=" flex flex-col items-center lg:items-start justify-center gap-2">
+                    <h1 className="text-white text-2xl font-medium mb-3">Social</h1>
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
+                        <Link href="" target="_blank">
+                        Instagram
+                        </Link>
+                    </li>
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
+                        <Link href="" target="_blank">
+                        Whatsapp
+                        </Link>
+                    </li>
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
+                        <Link href="" target="_blank">
+                        Linkedin
+                        </Link>
+                    </li>
+                    <li className="text-white/50 transition-all duration-700 hover:text-white hover:-translate-y-1">
+                        <Link href="" target="_blank">
+                        Facebook
                         </Link>
                     </li>
                 </ul>
