6e180fcdac64f0e49589b3b27e9a8d2a8814ebbe
Gustavo Gantois <gugagantois@gmail.com>
2025-03-18 21:52:00 -0300

    feat:Create service section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { CarouselTechs } from "./c-carousel-techs";
 import { HomeHero } from "./s-home";
+import { HomeServices } from "./s-services";
 
 export default function Page() {
   return (
     <>
     <HomeHero />
     <CarouselTechs />
+    <HomeServices />
     </>
   );
 }
---
+++ src/app/s-services.tsx
@@ -0,0 +1,11 @@
+export function HomeServices() {
+    return (
+        <section className="max-w-[1440px] mx-auto p-8 lg:p-0 mt-20 lg:mt-[100px]">
+            <div className="flex items-center justify-center flex-col gap-4">
+                <h1 className="bg-clip-text text-transparent text-4xl lg:text-6xl bg-gradient-to-r from-[#FFFFFF] to-[#999999] font-medium max-w-[816px] text-center">
+                Serviços que temos para oferecer na <span className="text-[#1D0DB1]"> GC Technology </span>
+                </h1>
+            </div>
+        </section>
+    )
+}
